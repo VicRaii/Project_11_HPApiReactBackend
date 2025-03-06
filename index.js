1353265782f56a486c06cb1557700f6209493069
VicRaii <vimarupon@hotmail.com>
2025-03-06 16:36:17 +0100

    fix server errors

--- index.js
+++ index.js
@@ -7,18 +7,7 @@ const charactersRoutes = require('./src/api/routes/characters')
 const app = express()
 
 app.use(express.json())
-app.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:4173',
-      'https://project-11-hp-api-react-backend.vercel.app',
-      'https://project-11-hpapi-react.netlify.app'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-  })
-)
+app.use(cors())
 
 connectDB()
 
