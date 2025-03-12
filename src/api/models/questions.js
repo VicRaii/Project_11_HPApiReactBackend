8f9e3e00f4726d4987205a7efcc47836c49fb1f5
VicRaii <vimarupon@hotmail.com>
2025-03-12 20:04:44 +0100

    add level key

--- src/api/models/questions.js
+++ src/api/models/questions.js
@@ -14,6 +14,10 @@ const QuestionSchema = new mongoose.Schema(
       type: Array,
       required: true
     },
+    level: {
+      type: Number,
+      required: true
+    },
     points: {
       type: Number,
       required: true
